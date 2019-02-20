7321a8c8fd1bbf76f7553060b2d6eacf84e740ef
netjoin <netjoin@gmail.com>
2019-02-21 03:07:18 +0800

    添加函数定义（别名）功能

--- src/functions.php
+++ src/functions.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace OpenSearch;
+
 /**
  * null合并运算符
  * 
@@ -25,4 +27,112 @@ function _isset($arr, $key = '', $value = null)
 
     // 低版本
     return isset($arr[$key]) ? $arr[$key] : $value;
+}
+
+function func($origin, $name)
+{
+	if (!isset($GLOBALS['FUNC_ARGS'])) {
+		$GLOBALS['FUNC_ARGS'] = [];
+	}
+	/*
+	if (!isset($GLOBALS['FUNC_ARGS'][$name])) {
+		$GLOBALS['FUNC_ARGS'][$name] = [];
+	}
+	*/
+	$GLOBALS['FUNC_ARGS'][] = $name;
+	$total = count($GLOBALS['FUNC_ARGS']);
+	$ns = '';
+	if (preg_match('/(.*)\\\([a-z_]+)$/i', $name, $matches)) {
+		# print_r($matches);
+		$ns = $matches[1];
+		$name = $matches[2];
+	}
+	$namespace = $ns;
+	$namespace = preg_replace('/^\\\|\\\$/', '', $namespace);
+	
+
+
+	$num = func_num_args();
+	$arr = [];
+	$params = [];
+	$alphabet = '_ abcdefghijklmnopqrstuvwxyz';
+	for ($i = 2; $i < $num; $i++) {
+		$j = $i - 2;
+		$str = '';
+		$value = func_get_arg($i);
+		if (is_bool($value)) {
+			$value = $value ? 'true' : 'false';
+
+		} elseif (is_null($value)) {
+			$value = 'null';
+
+		} elseif(is_string($value)) {
+			$value = "'$value'";
+
+		} elseif(is_numeric($value)) {
+			
+		} elseif(is_array($value)) {			
+			# $GLOBALS['FUNC_ARGS'][$name][$j] = $value;
+			$nm = "FUNC_ARGS_$total" . "_$j";
+			define($nm, $value);
+			# $value = "\$GLOBALS['FUNC_ARGS']['$name'][$j]";
+			$value = "$nm";
+		} else {
+			var_dump($value);
+			print_r([__FILE__, __LINE__]);
+			exit;
+		}
+		$key = $alphabet[$i];
+		$str = '$' . "$key = $value";
+		# echo $str . PHP_EOL;
+		$arr[] = $str;
+		$params[] = "\$$key";
+	}
+	$arg = implode(', ', $arr); #echo 
+	$param = implode(', ', $params);
+	# print_r($GLOBALS['FUNC_ARGS']);# 
+	$arr = [];
+	foreach ($GLOBALS as $k => $v) {
+		$arr[] = $k;
+	}
+	# print_r($arr); 
+	$str = "namespace $namespace { function $name($arg) { return $origin($param); } }"; #
+	$class = $name;
+	if ($namespace) {
+		$class = "\\$namespace\\$name";		
+	}
+	$origin = preg_replace('/^\\\|\\\$/', '', $origin);
+	
+	
+
+	$info = [
+		'namespace' => $namespace, 
+		'name' => $name, 
+		'class' => $class, 
+		'origin' => $origin, 
+		func_get_args(), 
+		__FILE__, 
+		'line' => __LINE__
+	];
+
+	if ($class == $origin) {
+		$info['line'] = __LINE__;
+		debug($str, $info, 1);
+	}
+	if (!function_exists($class)) {
+		eval($str);
+	} else {
+		$info['info'] = "Cannot redeclare $name()";
+		$info['line'] = __LINE__;
+		# debug($str, $info);
+	}
+}
+
+function debug($str, $arr, $exit = null)
+{
+	echo $str;
+	print_r($arr);
+	if ($exit) {
+		exit;
+	}
 }
