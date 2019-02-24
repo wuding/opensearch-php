50e486c5be92f65b8ba2b9741721239ebb5860b1
netjoin <netjoin@gmail.com>
2019-02-25 02:45:01 +0800

    添加 OpenSearch 自动发现链接，函数方法不再定义参数、自动获取参数数组或对象

--- src/Generate.php
+++ src/Generate.php
@@ -4,6 +4,12 @@ namespace OpenSearch;
 class Generate
 {
     public $osd = null;
+    public static $funcArgs = [
+        'link' => ['object,array', 'href' => null, 'title' => null],
+    ];
+    public static $argTypes = [
+        'link' => ['object', 'array'],
+    ];
 
     public function __construct($config = null)
     {
@@ -106,5 +112,78 @@ class Generate
         }
         $attr = implode(' ', $pieces);
         return $tag = "<$item $attr>$val</$item>";
+    }
+
+    public static function link()
+    {
+        $params = self::args(func_get_args(), __FUNCTION__);
+        # print_r($params);exit;
+        extract($params);
+        $href = $href ? : $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/opensearch';
+        # print_r($_SESSION);exit;
+        $key = function () use (&$title, $href) {
+            $title = trim($title);
+            if (!$title) {
+                $hash = md5($href);
+                if (isset($_SESSION[$hash])) {
+                    $title = $_SESSION[$hash];
+                } else {
+                    $contents = file_get_contents($href);
+                    $xmlObj = new \SimpleXMLElement($contents);
+                    $_SESSION[$hash] = $title = (string) $xmlObj->ShortName;
+                }
+            }
+            $title = htmlspecialchars($title);
+        };
+        $key();
+        return $key = '<link rel="search" type="application/opensearchdescription+xml" href="' . $href . "\" title=\"$title\" />";
+        print_r(get_defined_vars());exit;
+    }
+
+    public static function args()
+    {
+        $arg = func_get_args();
+        $args = $arg[0];
+        $arr = self::$funcArgs[$arg[1]];
+        # $types = self::$argTypes[$arg[1]];
+        $types = array_shift($arr);
+        $types = preg_split('/[,\s]+/', $types);
+        $keys = array_keys($arr);
+        $len = count($arr);
+        $params = $value = null;
+        if ($args) {
+            $value = $args[0];
+            if (is_string($value) && !in_array('json', $types)) {
+                $value = json_decode($value);
+                # print_r([__LINE__, __FILE__, $value]);exit;
+            } elseif (is_array($value) && in_array('array', $types)) {
+                $value = null;
+
+            } elseif (is_object($value) && in_array('object', $types)) {
+                $value = null;
+            }
+
+            if (is_object($value)) {
+                $value = (array) $value;
+            }
+
+            if (is_array($value)) {
+                $params = [];
+                foreach ($value as $key => $val) {
+                    if (is_numeric($key)) {
+                        $key = $keys[$key];
+                    }
+                    $params[$key] = $val;
+                }
+            }
+        }
+        if (null === $params) {
+            $params = [];
+            for ($i = 0; $i < $len; $i++) {
+                $k = $keys[$i];
+                $params[$k] = $args[$i];
+            }
+        }
+        return $params = array_merge($arr, $params);
     }
 }
